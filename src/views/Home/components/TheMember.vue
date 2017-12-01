2a046cda81ed40d5ef4f89effe5ed301f80ea2ca
Geeku <ggongpei@gmail.com>
2017-12-01 13:38:23 +0800

    [Wip]: Member area

--- src/views/Home/components/TheMember.vue
+++ src/views/Home/components/TheMember.vue
@@ -6,7 +6,9 @@
       </div>
     </header>
     <section class="main">
-      
+      <template v-for="member in members">
+        <MemberCard :member="member"></MemberCard>
+      </template>
     </section>
     <footer>
       <img src="static/img/Seedling.svg" alt="Seedling" aria-hidden="true">
@@ -15,10 +17,89 @@
 </template>
 
 <script>
+import MemberCard from './MemberCard.vue'
+
 export default {
   name: 'TheMember',
+  components: {
+    MemberCard
+  },
   data () {
     return {
+      members: [
+        {
+          name: '树',
+          description: '描述描述描述描述描述描述',
+          avatar: 'https://avatars2.githubusercontent.com/u/3984824?s=460&v=4',
+          tags: ['前端']
+        },
+        {
+          name: '树',
+          description: '描述描述描述描述描述描述',
+          avatar: 'https://avatars2.githubusercontent.com/u/3984824?s=460&v=4',
+          tags: ['前端']
+        },
+        {
+          name: '树',
+          description: '描述描述描述描述描述描述',
+          avatar: 'https://avatars2.githubusercontent.com/u/3984824?s=460&v=4',
+          tags: ['前端']
+        },
+        {
+          name: '树',
+          description: '描述描述描述描述描述描述',
+          avatar: 'https://avatars2.githubusercontent.com/u/3984824?s=460&v=4',
+          tags: ['前端']
+        },
+        {
+          name: '树',
+          description: '描述描述描述描述描述描述',
+          avatar: 'https://avatars2.githubusercontent.com/u/3984824?s=460&v=4',
+          tags: ['前端']
+        },
+        {
+          name: '树',
+          description: '描述描述描述描述描述描述',
+          avatar: 'https://avatars2.githubusercontent.com/u/3984824?s=460&v=4',
+          tags: ['前端']
+        },
+        {
+          name: '树',
+          description: '描述描述描述描述描述描述',
+          avatar: 'https://avatars2.githubusercontent.com/u/3984824?s=460&v=4',
+          tags: ['前端']
+        },
+        {
+          name: '树',
+          description: '描述描述描述描述描述描述',
+          avatar: 'https://avatars2.githubusercontent.com/u/3984824?s=460&v=4',
+          tags: ['前端']
+        },
+        {
+          name: '树',
+          description: '描述描述描述描述描述描述',
+          avatar: 'https://avatars2.githubusercontent.com/u/3984824?s=460&v=4',
+          tags: ['前端']
+        },
+        {
+          name: '树',
+          description: '描述描述描述描述描述描述',
+          avatar: 'https://avatars2.githubusercontent.com/u/3984824?s=460&v=4',
+          tags: ['前端']
+        },
+        {
+          name: '树',
+          description: '描述描述描述描述描述描述',
+          avatar: 'https://avatars2.githubusercontent.com/u/3984824?s=460&v=4',
+          tags: ['前端']
+        },
+        {
+          name: '树',
+          description: '描述描述描述描述描述描述',
+          avatar: 'https://avatars2.githubusercontent.com/u/3984824?s=460&v=4',
+          tags: ['前端']
+        }
+      ]
     }
   }
 }
@@ -32,5 +113,11 @@ export default {
     background-color: $yellow;
     @include flex-cross-center(flex, column);
     justify-content: space-between;
+  }
+  .main {
+    width: 100%;
+    @include flex-center;
+    flex-wrap: wrap;
+    align-content: center;
   }
 </style>
---
+++ src/views/Home/components/memberCard.vue
@@ -0,0 +1,80 @@
+<template>
+  <article class="memeber-card">
+    <img :src="member.avatar" :alt="member.name" class="avatar">
+    <section class="content">
+      <h4>{{ member.name }}</h4>
+      <p>{{ member.description }}</p>
+    </section>
+    <ul class="tags">
+      <li v-for="tag in member.tags">{{ `#${tag}` }}</li>
+    </ul>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'MemberCard',
+  props: ['member'],
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  @import '~style';
+
+  .memeber-card {
+    border-radius: .8vw;
+    @include flex-center;
+    padding: $gap-small;
+    width: 20%;
+    color: $brown;
+    margin: $gap-small;
+    font-weight: bolder;
+    position: relative;
+    border: 1px solid transparent;
+    .content {
+      flex: 1;
+    }
+    ul {
+      list-style: none;
+      position: absolute;
+      right: $gap-small;
+      top: $gap-small;
+      font-size: 12px;
+      background-color: $brown;
+      color: $yellow;
+      padding: 0 $gap-tiny;
+      line-height: 1.5;
+      border-radius: 5px;
+    }
+    img {
+      width: 50px;
+      height: 50px;
+      border-radius: 50%;
+      margin-right: $gap-small;
+      background: $brown;
+      box-shadow: 0 0 0 1px $brown;
+    }
+    h4 {
+      font-size: 20px;
+      font-weight: bolder;
+      color: inherit;
+      margin-bottom: $gap-tiny;
+    }
+    p {
+      font-size: 12px;
+    }
+    &:hover {
+      border-color: $brown;
+      box-shadow: 0 1px 0 $brown, 0 2px 0 $brown, 0 3px 0 $brown;
+      transform: translateY(-3px);
+      transition: transform .1s ease-in-out, box-shadow .1s ease-in-out;
+      img {
+        border-color: $brown;
+      }
+    }
+  }
+</style>
