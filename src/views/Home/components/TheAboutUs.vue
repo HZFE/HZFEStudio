5212244b59d00c19e1442a5652bb5ecea7d04951
goodhome <565149480@qq.com>
2017-12-03 16:48:43 +0800

    [Style]:change  and add fish animate

--- src/views/Home/components/TheAboutUs.vue
+++ src/views/Home/components/TheAboutUs.vue
@@ -1,25 +1,50 @@
 <template>
   <section class="home-section about-us">
-    <header>
+    <!--<header>
       <div class="rect-skew_blue">
         <h2>About</h2>
       </div>
-    </header>
+    </header>-->
     <section class="main">
-    
+      <article class="animate-container"
+               ref="animateContainer">
+      </article>
     </section>
     <footer>
-      
+
     </footer>
   </section>
 </template>
 
 <script>
+import Bodymovin from 'bodymovin'
 export default {
   name: 'TheAboutUs',
   data () {
     return {
     }
+  },
+  methods: {
+    init_fishAnimate () {
+      /**
+       * 鲤鱼打挺(x
+       * @author goodhome update(2017/12/3)
+       * */
+      let animateContainer = this.$refs.animateContainer
+      let animateConfig = {
+        container: animateContainer,
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'src/fish.json'
+      }
+      Bodymovin.loadAnimation(animateConfig)
+    }
+  },
+  created () {
+    this.$nextTick(() => {
+      this.init_fishAnimate()
+    })
   }
 }
 </script>
@@ -28,9 +53,18 @@ export default {
   @import '~style';
 
   .about-us {
+    position: relative;
     height: 100vh;
     background-color: $blue;
     @include flex-cross-center(flex, column);
     justify-content: space-between;
+    .animate-container {
+      position: absolute;
+      top: -182px;
+      left: 50%;
+      width: 420px;
+      height: auto;
+      transform: translateX(-50%);
+    }
   }
 </style>
